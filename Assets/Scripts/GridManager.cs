e1aaa802728e3483616707698f5730b90dee23db
Autumn <ao191@humboldt.edu>
2022-09-21 18:30:30 -0700

    Added button prefabs and imported alagard font as asset

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,6 +13,9 @@ public class GridManager : MonoBehaviour
     // the game camera
     [SerializeField] private Transform _camera;
 
+    // the dictionary containing all visible tiles
+    private Dictionary<Vector2, Tile> _tiles;
+
     void Start()
     {
         GenerateGrid();
@@ -20,15 +23,28 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        // create dictionary to hold spawned tiles
+        _tiles = new Dictionary<Vector2, Tile>();
+
+        // create "empty tile" objects
         for (int x = 0; x < _width; x++)
         {
             for (int y = 0; y < _height; y++)
             {
                 var tileSpawn = Instantiate(_emptyTilePrefab, new Vector3(x, y), Quaternion.identity);
                 tileSpawn.name = $"Tile {x} {y}";
+
+                _tiles[new Vector2(x, y)] = tileSpawn;
             }
         }
 
         //_camera.transform.position = new Vector3((float))
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out Tile returnedTile))
+            return returnedTile;
+        return null;
     }
 }
