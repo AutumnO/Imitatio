a99e8e78b6f68a8c8d9c189b90025514558e13e0
Autumn <ao191@humboldt.edu>
2022-09-23 15:46:23 -0700

    Added map creation tools hot keys, smoothed out panning with buttons

--- Assets/Scripts/InputControls.cs
+++ Assets/Scripts/InputControls.cs
@@ -9,27 +9,62 @@ public class InputControls : MonoBehaviour
     
     private Vector3 _camPosition;
     private float _camSize;
-    private PlayerInput _playerInput;
     private float _panSpeed;
     private float _zoomSpeed;
-    private float currentTime;
-    private float timeBetweenMovementIncrements;
     private Vector2 _panDirection;
     private bool _isPanning;
+    private bool _isPanningButton;
+
+    private PlayerInput _playerInput;
 
     // store controls
     private InputAction _panAction;
     private InputAction _zoomAction;
+    private InputAction _handToolAction;
+    private InputAction _hammerToolAction;
+    private InputAction _lightingToolAction;
+    private InputAction _copyToolAction;
+    private InputAction _undoToolAction;
+    private InputAction _redoToolAction;
 
     private void Awake()
     {
+        _camPosition = new Vector3(0, 0, 0);
+        _camSize = 5.0f;
+        _panSpeed = 0.4f;
+        _zoomSpeed = 0.75f;
+        _panDirection = new Vector2 ( 0, 0 );
+        _isPanning = false;
+        _isPanningButton = false;
+
         _playerInput = GetComponent<PlayerInput>();
         _panAction = _playerInput.actions["Pan"];
         _zoomAction = _playerInput.actions["Zoom"];
-        _panSpeed = 0.4f;
-        _zoomSpeed = 0.75f;
+        _handToolAction = _playerInput.actions["HandTool"];
+        _hammerToolAction = _playerInput.actions["HammerTool"];
+        _lightingToolAction = _playerInput.actions["LightingTool"];
+        _copyToolAction = _playerInput.actions["CopyTool"];
+        _undoToolAction = _playerInput.actions["UndoTool"];
+        _redoToolAction = _playerInput.actions["RedoTool"];
     }
 
+    private void FixedUpdate()
+    {
+        if (_isPanning || _isPanningButton)
+        {
+            if (_isPanning)
+                _panDirection = _panAction.ReadValue<Vector2>();
+            if (_panDirection.x == 1)
+                ChangeCameraPosition('E');
+            else if (_panDirection.x == -1)
+                ChangeCameraPosition('W');
+
+            if (_panDirection.y == 1)
+                ChangeCameraPosition('N');
+            else if (_panDirection.y == -1)
+                ChangeCameraPosition('S');
+        }
+    }
 
     // MAP CREATION
 
@@ -101,21 +136,35 @@ public class InputControls : MonoBehaviour
             _isPanning = false;
     }
 
-    private void FixedUpdate()
+    public void HandTool(InputAction.CallbackContext context)
     {
-        if (_isPanning)
-        {
-            _panDirection = _panAction.ReadValue<Vector2>();
-            if (_panDirection.x == 1)
-                ChangeCameraPosition('E');
-            else if (_panDirection.x == -1)
-                ChangeCameraPosition('W');
-
-            if (_panDirection.y == 1)
-                ChangeCameraPosition('N');
-            else if (_panDirection.y == -1)
-                ChangeCameraPosition('S');
-        }
+        if (context.performed)
+            OnClickToolsHand();
+    }
+    public void HammerTool(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnClickToolsHammer();
+    }
+    public void LightingTool(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnClickToolsLighting();
+    }
+    public void CopyTool(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnClickToolsCopy();
+    }
+    public void UndoTool(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnClickToolsUndo();
+    }
+    public void RedoTool(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            OnClickToolsRedo();
     }
 
     // Compass
@@ -127,21 +176,57 @@ public class InputControls : MonoBehaviour
     {
         ChangeCameraZoom(1);
     }
-    public void OnClickCompassNorth()
+    public void OnClickCompassNorth(bool buttonHeld)
     {
-        ChangeCameraPosition('N');
+        if (buttonHeld)
+        {
+            _isPanningButton = true;
+            _panDirection.Set(0, 1);
+        }
+        else
+        {
+            _isPanningButton = false;
+            _panDirection.Set(0, 0);
+        }
     }
-    public void OnClickCompassEast()
+    public void OnClickCompassEast(bool buttonHeld)
     {
-        ChangeCameraPosition('E');
+        if (buttonHeld)
+        {
+            _isPanningButton = true;
+            _panDirection.Set(1, 0);
+        }
+        else
+        {
+            _isPanningButton = false;
+            _panDirection.Set(0, 0);
+        }
     }
-    public void OnClickCompassSouth()
+    public void OnClickCompassSouth(bool buttonHeld)
     {
-        ChangeCameraPosition('S');
+        if (buttonHeld)
+        {
+            _isPanningButton = true;
+            _panDirection.Set(0, -1);
+        }
+        else
+        {
+            _isPanningButton = false;
+            _panDirection.Set(0, 0);
+        }
     }
-    public void OnClickCompassWest()
+    public void OnClickCompassWest(bool buttonHeld)
     {
-        ChangeCameraPosition('W');
+        if (buttonHeld)
+        {
+            _isPanningButton = true;
+            _panDirection.Set(-1, 0);
+        }
+        else
+        {
+            _isPanningButton = false;
+            _panDirection.Set(0, 0);
+        }
     }
 
     // Tools
