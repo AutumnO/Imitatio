35bd59d3ad403cc242304f8a536f53f2b9ef94c7
Autumn <ao191@humboldt.edu>
2022-09-24 02:19:17 -0700

    Added title card scene

---
+++ Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public void OnClickPlay()
+    {
+
+    }
+    public void OnClickMaps()
+    {
+
+    }
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
