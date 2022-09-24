12d3648c4f29b17184313d065e4f4e8c1a7caeca
Autumn <ao191@humboldt.edu>
2022-09-24 15:43:06 -0700

    Added scene loading to title screen, implemented basic loading bar

--- Assets/Scripts/TitleSceneManager.cs
+++ Assets/Scripts/TitleSceneManager.cs
@@ -1,16 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleSceneManager : MonoBehaviour
 {
-    public void OnClickPlay()
-    {
+    [SerializeField] private GameObject loadingBarObject;
+    
+    private Slider _loadingBar;
+    private TextMeshProUGUI _progressText;
 
+    private void Awake()
+    {
+        _loadingBar = loadingBarObject.GetComponent<Slider>();
+        _progressText = loadingBarObject.GetComponentInChildren<TextMeshProUGUI>();
     }
-    public void OnClickMaps()
-    {
 
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingBarObject.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            _loadingBar.value = progress;
+            _progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+    public void OnClickLoadScene(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
     }
     public void OnClickExit()
     {
