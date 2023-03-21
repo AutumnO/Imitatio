4e8a8784a574b78d004cb72b1df7fab466339cbd
Autumn <aut.ossello@gmail.com>
2023-03-21 14:12:08 -0700

    Optimized the character creation toggle listener functions, now clicking on any of the existing carosel items causes the avatar to change

--- Assets/Scripts/UIManagers/CharacterCreationUIManager.cs
+++ Assets/Scripts/UIManagers/CharacterCreationUIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,10 +10,16 @@ public class CharacterCreationUIManager : MonoBehaviour
     [SerializeField] private GameObject _caroselItemPrefab;
     [SerializeField] private GameObject _mainAvatar;
     [SerializeField] private GameObject _bodyTypePanel;
+    [SerializeField] private GameObject _eyebrowsToggleGroupObj;
+    [SerializeField] private GameObject _eyesToggleGroupObj;
+    [SerializeField] private GameObject _mouthToggleGroupObj;
 
     public void Start()
     {
-        AddBodyTypeListeners();
+        AddAvatarListener(_bodyTypePanel, BasicAssetType.Body);
+        AddAvatarListener(_eyebrowsToggleGroupObj, BasicAssetType.Eyebrow);
+        AddAvatarListener(_eyesToggleGroupObj, BasicAssetType.Eyes);
+        AddAvatarListener(_mouthToggleGroupObj, BasicAssetType.Mouth);
     }
 
     public AvatarCosmeticsList GetCaroselObjects()
@@ -24,24 +31,24 @@ public class CharacterCreationUIManager : MonoBehaviour
     {
         return _caroselItemPrefab;
     }
-
-    // Listeners
     
-    // Add Toggle Listeners
-    private void AddBodyTypeListeners()
+    // Add listeners to toggle groups to change features on the main avatar when things are selected
+
+    private void AddAvatarListener(GameObject toggleGroupObj, BasicAssetType type)
     {
-        Toggle[] toggles = _bodyTypePanel.GetComponentsInChildren<Toggle>();
-        foreach(Toggle toggle in toggles)
+        Toggle[] toggles = toggleGroupObj.GetComponentsInChildren<Toggle>();
+        foreach (Toggle toggle in toggles)
         {
-            toggle.onValueChanged.AddListener(delegate { ChangeBody(toggle); });
+            toggle.onValueChanged.AddListener(delegate { ChangeFeature(toggle, type); });
         }
     }
-    private void ChangeBody(Toggle toggle)
+
+    private void ChangeFeature(Toggle toggle, BasicAssetType type)
     {
         if(toggle.isOn == true)
         {
             BasicAsset asset = toggle.gameObject.GetComponent<AssetComponent>().asset;
-            _mainAvatar.GetComponent<Avatar>().SetAsset(asset, BasicAssetType.Body);
+            _mainAvatar.GetComponent<Avatar>().SetAsset(asset, type);
         }
     }
 }
